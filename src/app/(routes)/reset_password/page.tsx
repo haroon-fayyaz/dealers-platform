71e8c025e761ae6389ce855228a3bb3001b41e78
Haroon Fayyaz <haroonfayyaz17@gmail.com>
2024-10-24 17:26:54 +0500

    Reset Password page implemented

---
+++ src/app/(routes)/reset_password/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+  AuthButton,
+  AuthFooterText,
+  FormWrapper,
+  PasswordInput,
+} from '@/app/components/Auth';
+import { FieldContainer } from '@/app/components/Common/FieldWrapper';
+import { CustomLink } from '@/app/components/Common/CustomLink';
+import { PASSWORD_REGEX } from '@/utils/constants';
+
+export default function ForgotPassword() {
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      confirm_password: '',
+    },
+    validationSchema: Yup.object({
+      password: Yup.string()
+        .matches(
+          PASSWORD_REGEX,
+          'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character'
+        )
+        .required('Password is required'),
+      confirm_password: Yup.string()
+        .oneOf([Yup.ref('password')], 'Passwords must match')
+        .required('Confirm Password is required'),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <FormWrapper
+        title="Forgot Password"
+        footer={
+          <AuthFooterText>
+            Back to <CustomLink href="/login">Sign In</CustomLink>
+          </AuthFooterText>
+        }
+      >
+        <FieldContainer wrapperClassName="mt-4">
+          <p className="text-sm text-custom-gray">
+            Enter registered email address to change your password.
+          </p>
+        </FieldContainer>
+        <FieldContainer wrapperClassName="mt-8">
+          <PasswordInput
+            label="Password"
+            {...formik.getFieldProps('password')}
+            error={formik.touched.password && formik.errors.password}
+          />
+          <PasswordInput
+            label="Confirm Password"
+            {...formik.getFieldProps('confirm_password')}
+            error={
+              formik.touched.confirm_password && formik.errors.confirm_password
+            }
+          />
+        </FieldContainer>
+        <AuthButton type="submit">Reset Password</AuthButton>
+      </FormWrapper>
+    </form>
+  );
+}
